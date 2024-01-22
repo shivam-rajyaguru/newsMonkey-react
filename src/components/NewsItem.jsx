ff1c7d9eaa3fe2ad9967c502203e61a0178b74e1
shiva <guru_shiva1625@gmail.com>
2024-01-22 10:44:30 +0530

    understanding state and constructor and add json file

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,20 +2,18 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, descryption } = this.props;
+    let { title, descryption, url } = this.props;
     return (
-      <div className="card my-4" style={{ width: "18rem" }}>
-        <img
-          src="https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/ZLS7UQ33ORFKHAU2LRCF6LVBHY.jpg&w=1440"
-          className="card-img-top"
-          alt="..."
-        />
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{descryption}</p>
-          <a href="/" className="btn btn-primary">
-            Go somewhere
-          </a>
+      <div className="my-4">
+        <div className="card my-3" style={{ width: "18rem" }}>
+          <img src={url} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{descryption}</p>
+            <a href="/" className="btn btn-primary">
+              Read More
+            </a>
+          </div>
         </div>
       </div>
     );
